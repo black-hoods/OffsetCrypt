f0b11ba3e88ac36310acfc96ce2190bdd21d500c
tsbits <contact@tsbits.net>
2022-07-31 17:40:58 +0200

    crypt() done

--- OffsetCrypt.js
+++ OffsetCrypt.js
@@ -1,22 +1,23 @@
 class OffsetCrypt{
-	static regularCharMatrix = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0'];
+	static regularCharMatrix = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','1','2','3','4','5','6','7','8','9','0',' '];
 
-	static crypt(message, offset = 0){
-		let shiftedMatrix = [...regularCharMatrix];
+	static crypt(message = "Hello world", options = { offset: 42, charMatrix: [...OffsetCrypt.regularCharMatrix] }){
+		let regularMatrix = options.charMatrix;
+		let shiftedMatrix = [...regularMatrix];
+		let cryptedMessage = '';
 
-		for(let i = 0; i < offset; i++){
+		for(let i = 0; i < options.offset; i++){
 			shiftedMatrix.push(shiftedMatrix.shift());
 		}
 
-		for(let char in cleanedMessage){
-			let n = MorseJS.regularCharMatrix[cleanedMessage[char].toLowerCase()];
+		for(let char in message){
+			let regularMatrixIndex = regularMatrix.indexOf(message[char].toLowerCase());
+			let shiftedMatrixChar = shiftedMatrix[regularMatrixIndex];
 
-			if(n != null){
-				if(char > 0){
-					cryptedMessage += ' ';
-				}
+			console.log(regularMatrixIndex, shiftedMatrixChar);
 
-				cryptedMessage += n;
+			if(shiftedMatrixChar != null){
+				cryptedMessage += shiftedMatrixChar;
 			}
 		}
 
